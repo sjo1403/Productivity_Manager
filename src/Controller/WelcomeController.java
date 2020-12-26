a5398ab2505c3d6a704a286f3e53852a5500ba8b
shanteljohnson <sjo1403@wgu.edu>
2020-12-25 22:18:35 -0500

    Improved app functionality.

--- src/Controller/WelcomeController.java
+++ src/Controller/WelcomeController.java
@@ -1,4 +1,5 @@
 package Controller;
+import Model.Timecard;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +10,9 @@ import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WelcomeController {
 
@@ -39,6 +43,12 @@ public class WelcomeController {
      */
     @FXML
     void newButt(ActionEvent event) throws IOException {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        Timecard newTimecard = new Timecard("Shantel's GoodCard", dateFormat.format(date));
+        System.out.println(newTimecard.getCardDate());
+
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
         stage.setTitle("GoodCard");
